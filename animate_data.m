f585f9da05162e7edbfc29b3f44b5dbb829e87c7
Matthew Brucker <mbrucker@olin.edu>
2016-11-28 16:00:19 -0500

    Added animation function

--- animate_data.m
+++ animate_data.m
@@ -1,26 +1,58 @@
-function animate_data(t,state_vars)
+function animate_data()
     clf;
-    w = 1;
-    h = .2;
+    w = 1; % 1/2 Width of cart (m)
+    h = .2; % 1/2 Height of cart (m)
+    
+    w_p = .05; % 1/2 width of pendulum (m)
+    h_p = .3; % height of pendulum (m)
     
     
+    [t,out] = run_pendulum(0,3);
+    x_vals = out(:,1);
+    thetas = out(:,2)-pi/2; % Gets the out angles as standardized angles
     
-    x_center = 0;
-    y_center = .2;
-    x = [x_center-w x_center+w x_center+w x_center-w];
-    y = [y_center+h y_center+h y_center-h y_center-h];
+    
+    x_center = 0; % X center of the cart
+    y_center = .2; % Y center of the cart
+    x = [x_center-w x_center+w x_center+w x_center-w]; % X points for the cart
+    y = [y_center+h y_center+h y_center-h y_center-h];  % Y points for the cart
+    
+    % X and Y values of rotation point
+    xp_center = x_center;
+    yp_center = y_center + h + w_p;
+%     x_p = [xp_center - w_p, xp_center - w_p, xp_center + w_p, xp_center + w_p];
+%     y_p = [yp_center, yp_center + h_p, yp_center + h_p, yp_center];
+    x_p = [xp_center xp_center];
+    y_p = [yp_center yp_center + h_p];
+    
     hold on;
     cart = fill(x,y,'r');
+    pendulum = fill(x_p,y_p,'b','LineWidth',0.01);
     hold off;
-    axis([-3 3 0 6]);
+    axis([-6 6 0 6]);
 
-    for j = 1:length(t)
-          x_center = state_vars(j);
+    % Iterate across the time interval, animating the shapes
+    for j = 1:length(t)-1
+          x_center = x_vals(j);
+          % Calculate new vertices for this frame
           x = [x_center-w x_center+w x_center+w x_center-w];
           y = [y_center+h y_center+h y_center-h y_center-h];
           new_vertices = transpose(vertcat(x,y));
           cart.Vertices = new_vertices;
-          drawnow 
+          
+          % Calculate the new pendulum vertex values
+          theta = thetas(j);
+          xp_center = x_center;
+          x_p = [xp_center xp_center+h_p*cos(theta)];
+          y_p = [yp_center yp_center+h_p*sin(theta)];
+%           x_p = [xp_center-w*cos(theta),xp_center+h_p*cos(theta)-w*cos(theta), ...
+%               xp_center+h_p*cos(theta)+w*cos(theta),xp_center+w*cos(theta)];
+%           y_p = [yp_center+w*sin(theta),yp_center+h_p*sin(theta)+w*sin(theta), ...
+%               yp_center+h_p*sin(theta)-w*sin(theta),yp_center-w*sin(theta)];
+          new_pen_vertices = transpose(vertcat(x_p,y_p));
+          pendulum.Vertices = new_pen_vertices;
+          drawnow limitrate
+          pause(t(j+1)-t(j));
     end
     
 end
--- run_pendulum.m
+++ run_pendulum.m
@@ -1,21 +1,21 @@
-function res = run_pendulum(initial_time, final_time)
+function [t,res] = run_pendulum(initial_time, final_time)
     x_init = 0; % m
-    theta_init = 0; % rad
+    theta_init = pi; % rad
     
-    v_x_init = 2; % m/s
-    v_theta_init = .001; % rad/s
+    v_x_init = 1; % m/s
+    v_theta_init = 0; % rad/s
     
     state_vars_init = [x_init; theta_init; v_x_init; v_theta_init];
     
-    M = .5; % mass of cart, kg
-    m = .2; % mass of pendulum, kg
+    M = 10; % mass of cart, kg
+    m = 5; % mass of pendulum, kg
     b = .1; % coefficient of friction for cart, N/m/sec
     l = .3; % length toi pendulum center of mass, m
     I = .006; % moment of inertia of the pendulum, kg*m^2
     F = 0; % force applied to cart, N
     
     dxdt = @(ti, state_vars) get_state_vars(ti, state_vars, M, m, b, l, I, F);
-    [~, state_vars] = ode45(dxdt, [initial_time final_time], state_vars_init);
+    [t, state_vars] = ode45(dxdt, [initial_time final_time], state_vars_init);
     
     res = state_vars;
 end
