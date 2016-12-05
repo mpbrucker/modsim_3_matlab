3bb4c01c03bc3b853237771bce5ecc4c32b65ca0
Sophia Nieslen <sophianielsen22b@gmail.com>
2016-12-05 16:28:23 -0500

    add mass sweep

--- mass_sweep.m
+++ mass_sweep.m
@@ -4,11 +4,11 @@ final_time = 10;
 length_p = 3; % m
 width = .3; % m
 timestep = .001; % s
-k_p = 8000;
-k_i = 5000;
-k_d = 0;
+k_p = 9600;
+k_i = 100;
+k_d = 1100;
 
-mass_person_range = 45:5:90;
+mass_person_range = 45:15:90;
 mass_length = length(mass_person_range);
 
 for i = 1:mass_length
@@ -17,6 +17,10 @@ for i = 1:mass_length
     theta = res(:,2);
     theta = theta - pi;
     plot(t, theta);
+    xlabel('Time (s)');
+    ylabel('Deviation from upright (rad)');
+    legend('45 kg', '60 kg', '75 kg', '90 kg', 'location', 'Best');
+    title('Deviation from upright over range of masses');
     
 end
 
